0ae2a878bd7e7e1d58546b681f59750634d74f45
gregyjames <madra.uc@gmail.com>
2015-04-24 10:53:49 -0500

    Added project

---
+++ GEdit/GEdit/Settings.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace GEDITER
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(95, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(95, 68);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Foreground Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Background Color";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 124);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 50);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Font Family";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(95, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 180);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(76, 50);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Font Size";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(95, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 7;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(13, 260);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(259, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Save Settings";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 295);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Settings";
+            this.Text = "Settings";
+            this.Load += new System.EventHandler(this.Settings_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+    }
+}
---
+++ GEdit/GEdit/Settings.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace GEDITER
+{
+    public partial class Settings : Form
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ColorPick = new ColorDialog();
+
+            ColorPick.ShowDialog();
+
+            pictureBox1.BackColor = ColorPick.Color;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var ColorPick = new ColorDialog();
+
+            ColorPick.ShowDialog();
+
+            pictureBox2.BackColor = ColorPick.Color;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var FontName = new FontDialog();
+
+            FontName.ShowDialog();
+
+            label1.Text = FontName.Font.FontFamily.Name;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var FontName = new FontDialog();
+
+            FontName.ShowDialog();
+
+            label2.Text = FontName.Font.Size.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var SSettings = new settings
+                {
+                    R1 = pictureBox1.BackColor.R,
+                    G1 = pictureBox1.BackColor.G,
+                    B1 = pictureBox1.BackColor.B,
+                    R2 = pictureBox2.BackColor.R,
+                    G2 = pictureBox2.BackColor.G,
+                    B2 = pictureBox2.BackColor.B,
+                    FontName = label1.Text,
+                    FontSize = label2.Text
+                };
+            var x = new XmlSerializer(SSettings.GetType());
+
+            var file = new StreamWriter(@"settings.xml");
+
+            x.Serialize(file, SSettings);
+
+            
+            file.Close();
+        }
+
+        private void Settings_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(@"settings.xml"))
+            {
+                var mySerializer = new XmlSerializer(typeof(settings));
+
+                var myFileStream = new FileStream("settings.xml", FileMode.Open);
+
+                var myObject = (settings)mySerializer.Deserialize(myFileStream);
+
+                pictureBox1.BackColor = Color.FromArgb(myObject.R1, myObject.G1, myObject.B1);
+                pictureBox2.BackColor = Color.FromArgb(myObject.R2, myObject.G2, myObject.B2);
+                label1.Text = myObject.FontName;
+                label2.Text = myObject.FontSize;
+
+                myFileStream.Close();
+            }
+        }
+    }
+}
+
+public class settings
+    {
+        public int R1 = 0;
+        public int G1 = 0;
+        public int B1 = 0;
+        public int R2 = 0;
+        public int G2 = 0;
+        public int B2 = 0;
+        public string FontName = "";
+        public string FontSize = "0";
+    }
